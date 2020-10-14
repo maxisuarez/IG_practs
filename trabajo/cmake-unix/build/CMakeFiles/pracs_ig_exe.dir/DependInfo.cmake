cb062e4994bcfe36374998b3a484f73f4eba800e
maxisuarez <maxisuarez@corrreo.ugr.es>
2020-10-14 14:25:16 +0200

    first commit

---
+++ trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maxi/Escritorio/5.1/IG/recursos/src/cauce.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/cauce.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/recursos/src/fbo.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/fbo.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/recursos/src/ig-aux.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/ig-aux.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/recursos/src/lector-jpg.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/lector-jpg.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/recursos/src/lector-ply.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/lector-ply.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/recursos/src/matrices-tr.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/matrices-tr.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/animacion.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/animacion.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/array-verts.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/array-verts.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/camara.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/camara.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/escena.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/escena.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/grafo-escena.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/grafo-escena.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/main.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/main.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/malla-ind.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/malla-ind.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/malla-revol.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/malla-revol.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/materiales-luces.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/materiales-luces.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/objeto3d.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/objeto3d.cpp.o"
+  "/home/maxi/Escritorio/5.1/IG/trabajo/src/seleccion.cpp" "/home/maxi/Escritorio/5.1/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/seleccion.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../trabajo/include"
+  "../../../recursos/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/objeto3d.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/malla-revol.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/grafo-escena.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/malla-ind.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/escena.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/camara.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/main.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/seleccion.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/animacion.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/materiales-luces.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/trabajo/src/array-verts.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/matrices-tr.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/lector-ply.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/fbo.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/lector-jpg.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/ig-aux.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/maxi/Escritorio/5.1/IG/recursos/src/cauce.cpp.o"
+  "pracs_ig_exe.pdb"
+  "pracs_ig_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pracs_ig_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
